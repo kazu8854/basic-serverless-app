940325b6a245350f38a5463cd9c605aec61eb072
kazu8854 <kazu8854@users.noreply.github.com>
2026-03-28 22:10:01 +0900

    feat: add CloudFront/S3 SPA hosting, CDK Database Seeder trigger, and Cost estimation documentation

--- packages/infrastructure/lib/infrastructure-stack.ts
+++ packages/infrastructure/lib/infrastructure-stack.ts
@@ -7,6 +7,11 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as triggers from 'aws-cdk-lib/triggers';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -61,10 +66,59 @@ export class InfrastructureStack extends cdk.Stack {
       },
     });
 
+    // 5. Frontend SPA Hosting (S3 + CloudFront)
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html', // Redirect 404 to index.html for SPA routing
+        },
+      ],
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../frontend/dist'))],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ['/*'], // Invalidate cache on deploy
+    });
+
+    // 6. DB Migration / Seeder (CDK Trigger)
+    // Runs automatically upon `cdk deploy` to seed the database or execute migrations.
+    const seederLambda = new NodejsFunction(this, 'DbSeeder', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, '../../backend/src/seeder.ts'),
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(seederLambda);
+
+    new triggers.Trigger(this, 'SeederTrigger', {
+      handler: seederLambda,
+      timeout: cdk.Duration.minutes(2),
+      invocationType: triggers.InvocationType.EVENT,
+    });
+
     // Outputs for the Frontend
     new cdk.CfnOutput(this, 'ApiEndpointOutput', { value: api.url });
     new cdk.CfnOutput(this, 'UserPoolIdOutput', { value: userPool.userPoolId });
     new cdk.CfnOutput(this, 'UserPoolClientIdOutput', { value: userPoolClient.userPoolClientId });
     new cdk.CfnOutput(this, 'RegionOutput', { value: this.region });
+    new cdk.CfnOutput(this, 'FrontendUrlOutput', { value: `https://${distribution.distributionDomainName}` });
   }
 }
